b0b0ec9b000fe63dc2added1488ed1a7024159c8
Shafiya Heena <heena.shafiya@gmail.com>
2021-10-27 08:58:11 +0530

    updated files

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # expressportfolio
 Assignment 1 for COMP229 Web Dev
 https://comp229-shafiya-assign1.herokuapp.com/
+
+Assignment2 Business 
+https://comp229assign2shafiya.herokuapp.com/
--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/business_contact"
-    "URI": "mongodb+srv://1234:1234@cluster0.vq5ki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://1234:1234@cluster0.1kc3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
