c34054eb92ea0faa2a7bf7b6c10658f7c6dba728
Shafiya Heena <heena.shafiya@gmail.com>
2021-10-27 08:41:31 +0530

    Deploy to Heroku

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -100,7 +100,7 @@ module.exports.processLoginPage = (req, res, next) => {
             }
 
             const authToken = jwt.sign(payload, DB.Secret, {
-                expiresIn: 604800 // 1 week
+                expiresIn: 5 // 1 week
            });
 
             /* TODO - Getting Ready to convert to API
