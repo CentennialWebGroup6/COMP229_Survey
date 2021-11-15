d249715b24ad5bc1863f096676545c291a0c35a3
Shafiya Heena <heena.shafiya@gmail.com>
2021-11-16 01:28:06 +0530

    Push app to Git

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,4 @@
-//File Name: index.js
-//Student Name: Shafiya Heena
-//Student ID: 301199792 
-//Date:4/10/21
+
 var express = require('express');
 var router = express.Router();
 
@@ -16,45 +13,17 @@ router.get('/home', function(req, res, next) {
 });
 
 /* GET About Us page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About'});
+router.get('/activesurvey', function(req, res, next) {
+  res.render('activesurvey', { title: 'ActiveSurvey'});
 });
 
 /* GET Projects page. */
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects'});
+router.get('/createsurvey', function(req, res, next) {
+  res.render('createsurvey', { title: 'CreateSurvey'});
 });
 
-/* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services'});
-});
-
-/* GET Contact Us page. */
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact'});
-});
-
-/* navigate to Services page. */
-router.get('/serviceregister', function(req, res, next) { //routing to service page after service registeration button is clicked. 
-  res.render('serviceregister', { title: 'Register'});
-});
 
 let indexController = require('../controllers/index');
 
-/* GET Route for displaying the Login page */
-router.get('/login', indexController.displayLoginPage);
-
-/* POST Route for processing the Login page */
-router.post('/login', indexController.processLoginPage);
-
-/* GET Route for displaying the Register page */
-router.get('/register', indexController.displayRegisterPage);
-
-/* POST Route for processing the Register page */
-router.post('/register', indexController.processRegisterPage);
-
-/* GET to perform UserLogout */
-router.get('/logout', indexController.performLogout);
 
 module.exports = router;
